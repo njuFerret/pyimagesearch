14dd0b56cb10fae3c5680e717a54c788c5e5a58f
njuFerret <emagazi.cn@gmail.com>
2024-06-08 17:55:46 +0800

    google-images-deep-learning

---
+++ google-images-deep-learning/README.md
@@ -0,0 +1,2 @@
+# Deep Learning and Google Images for Training Data
+- Repository for [Deep Learning and Google Images for Training Data](https://www.pyimagesearch.com/2017/12/04/how-to-create-a-deep-learning-dataset-using-google-images/) tutorial.
---
+++ google-images-deep-learning/get-api-urls.py
@@ -0,0 +1,15 @@
+# from googlesearch import search
+#
+# # Search query
+# query = "foot from top view"
+#
+# # Get URL links
+# links = []
+# for j in search(query, tld="co.in", num=100, stop=100, pause=2):
+#     print(j)
+#     links.append(j)
+#
+# with open("api-urls.txt", "w") as txt_file:
+#     for line in links:
+#         txt_file.write("".join(line) + "\n")
+
